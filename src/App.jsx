b0db91fff4ccbc0d0126021109b6eb46174d2798
seochaemin13-ui <seochaemin13@gmail.com>
2026-02-09 06:27:20 +0900

    전체 홈페이지 디자인 구상 및 상품 카드 배치

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Card from './Card';
+import './App.css';
 
 function App() {
   const handleButtonClick = (title) => {
@@ -8,26 +9,36 @@ function App() {
 
   const cardsData = [
     { id: 1, title: "브랜드A", price: "35,000원", description: "편안하고 착용감이 좋은 신발", imageUrl: 'img/1.jpg', buttonText: "담기" },
+    { id: 2, title: "브랜드A", price: "25,000원", description: "힙한 컬러가 매력적인 신발", imageUrl: 'img/2.jpg', buttonText: "담기" },
+    { id: 3, title: "브랜드B", price: "35,000원", description: "힙한 컬러가 매력적인 신발", imageUrl: 'img/3.jpg', buttonText: "담기" }
     ];
 
   return (
-    <div style={{ 
-        display: 'flex', 
-        flexWrap: 'wrap', 
-        justifyContent: 'center', 
-        padding: '20px' 
-    }}>
-      {cardsData.map((card) => (
-        <Card
-          key={card.id}
-          title={card.title}
-          description={card.description}
-          price={card.price}
-          imageUrl={card.imageUrl}
-          buttonText={card.buttonText}
-          onButtonClick={() => handleButtonClick(card.title)}
-        />
-      ))}
+    <div className="App">{}
+      <header className="header-bar">
+        <div className="header-content">
+          <img src="/img/cart_icon.png"
+          alt="장바구니"
+          className='cart_icon'></img>
+        </div>
+      </header>{}
+      <div className="title-area">
+        <h1>신발 상품 목록</h1>
+        <p>현재 {cardsData.length}개의 상품이 있습니다.</p>
+      </div>{}
+      <div className="card_container">
+        {cardsData.map((card) => (
+          <Card
+            key={card.id}
+            title={card.title}
+            description={card.description}
+            price={card.price}
+            imageUrl={card.imageUrl}
+            buttonText={card.buttonText}
+            onButtonClick={() => handleButtonClick(card.title)}
+          />
+        ))}
+      </div>
     </div>
   );
 }
