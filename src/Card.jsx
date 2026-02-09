1e0db50aff1993cb5e0ad55edb85a58c11c7fd95
seochaemin13-ui <seochaemin13@gmail.com>
2026-02-10 04:56:16 +0900

    장바구니 숫자, 담기버튼 활성화 및 모바일 ui 맞춤

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,19 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './Card.css';
 
 const Card = ({ title, price, description, imageUrl, buttonText, onButtonClick }) => {
+ 
+  const[isAdded, setIsAdded]=useState(false);
+
+  const handleButtonClick=()=>{
+    if(!isAdded) {
+      setIsAdded(true);
+      if (onButtonClick){
+        onButtonClick();
+      }
+    }
+  }
+ 
   return (
     <div className="card">
       {imageUrl && <img src={imageUrl} alt={title} className="card-image" />}
@@ -9,9 +21,11 @@ const Card = ({ title, price, description, imageUrl, buttonText, onButtonClick }
         <h2 className="card-title">{title}</h2>
         <p className="card-description">{description}</p>
         <p className="card-price">{price}</p>
+
         {buttonText && (
-          <button className="card-button" onClick={onButtonClick}>
-            {buttonText}
+          <button className={`card-button ${isAdded ? "added":""}`}
+          onClick={handleButtonClick}>
+            {isAdded ? '담김!' : buttonText}
           </button>
         )}
       </div>
