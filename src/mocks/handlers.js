1fc4a7ac0d327d010c44a4b9412524bf1a54b2be
seochaemin13-ui <seochaemin13@gmail.com>
2026-03-12 04:04:22 +0900

    msw 라이브러리 설치

---
+++ src/mocks/handlers.js
@@ -0,0 +1,11 @@
+import { http, HttpResponse } from 'msw';
+
+export const handlers = [
+  http.get('/api/products', () => {
+    return HttpResponse.json([
+      { id: 1, title: "브랜드A", price: "35,000원", description: "편안하고 착용감이 좋은 신발", imageUrl: 'img/1.jpg', buttonText: "담기" },
+      { id: 2, title: "브랜드A", price: "25,000원", description: "힙한 컬러가 매력적인 신발", imageUrl: 'img/2.jpg', buttonText: "담기" },
+      { id: 3, title: "브랜드B", price: "35,000원", description: "힙한 컬러가 매력적인 신발", imageUrl: 'img/3.jpg', buttonText: "담기" }
+    ]);
+  }),
+];
