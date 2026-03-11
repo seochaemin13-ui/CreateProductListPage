7b4d2422b859edf94beb3589f07c1a427d270d72
seochaemin13-ui <seochaemin13@gmail.com>
2026-03-12 04:05:08 +0900

    recoil 라이브러리 설치

---
+++ src/recoil/atoms.js
@@ -0,0 +1,16 @@
+import { atom } from 'recoil';
+
+export const productsState = atom({
+  key: 'productsState',
+  default: [],
+});
+
+export const cartState = atom({
+  key: 'cartState',
+  default: JSON.parse(localStorage.getItem('cartItems')) || [],
+});
+
+export const cardsState = atom({
+  key: 'cardsState',
+  default: JSON.parse(localStorage.getItem('myCards')) || [],
+});
